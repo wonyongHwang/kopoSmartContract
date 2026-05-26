3fd23d89d1c77c6efa487f2cfd289f7397598208
Wayne Hwang <wonyong@wonyong.net>
2026-05-26 14:23:51 +0900

    Update NFT.sol

--- 3_ERC721_NFT/NFT.sol
+++ 3_ERC721_NFT/NFT.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts@4.9.6/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
