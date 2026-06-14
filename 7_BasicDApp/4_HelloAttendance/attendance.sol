7d424d9c8ef098dfe5ee9e845837ae5bd135a237
Wayne Hwang <wonyong@wonyong.net>
2026-06-14 12:42:09 +0900

    Update attendance.sol

--- 7_BasicDApp/4_HelloAttendance/attendance.sol
+++ 7_BasicDApp/4_HelloAttendance/attendance.sol
@@ -5,15 +5,11 @@ contract AttendanceCheck {
     mapping(address => bool) private checkedIn;
     mapping(address => uint256) private checkInTime;
 
-    event CheckIn(address indexed user, uint256 time);
-
     function checkIn() public {
         require(!checkedIn[msg.sender], "Already checked in");
 
         checkedIn[msg.sender] = true;
         checkInTime[msg.sender] = block.timestamp;
-
-        emit CheckIn(msg.sender, block.timestamp);
     }
 
     function isCheckedIn(address user) public view returns (bool) {
